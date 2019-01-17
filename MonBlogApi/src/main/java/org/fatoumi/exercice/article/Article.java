1c886a53ecab46271c65e7fda81634768cc4737a
Mahmoud Fatoumi <fatoumimahmoud@gmail.com>
2019-01-17 12:09:23 +0100

    Add rest controller for article with mock data

--- MonBlogApi/src/main/java/org/fatoumi/exercice/article/Article.java
+++ MonBlogApi/src/main/java/org/fatoumi/exercice/article/Article.java
@@ -5,9 +5,16 @@ import lombok.Data;
 @Data
 public class Article {
 
+    private Integer id;
     private String title;
+    private String content;
 
-    public Article(String title) {
+    public Article() {
+    }
+
+    public Article(Integer id, String title, String content) {
+        this.id = id;
         this.title = title;
+        this.content = content;
     }
 }
---
+++ MonBlogApi/src/main/java/org/fatoumi/exercice/article/service/ArticleService.java
@@ -0,0 +1,40 @@
+package org.fatoumi.exercice.article.service;
+
+import org.fatoumi.exercice.article.Article;
+
+import java.util.List;
+
+public interface ArticleService {
+
+    /**
+     *
+     * @return list off all articles
+     */
+    List<Article> findAll();
+
+    /**
+     *
+     * @param id article id
+     * @return the article with the id
+     */
+    Article find(Integer id);
+
+    /**
+     *
+     * @param article the article without id
+     * @return the created article
+     */
+    Article create(Article article);
+    /**
+     *
+     * @param article
+     * @return the edited article
+     */
+    Article edit(Article article);
+
+    /**
+     *
+     * @param id article id to be deleted
+     */
+    void delete(Integer id);
+}
