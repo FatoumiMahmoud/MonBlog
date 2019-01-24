f070be7954a19eb55502456bf4e0ea0c20061e5f
Mahmoud Fatoumi <fatoumimahmoud@gmail.com>
2019-01-24 11:24:28 +0100

    Fix commentControllerIntegrationTest class

--- MonBlogApi/src/test/java/org/fatoumi/exercice/controller/CommentControllerIntegrationTest.java
+++ MonBlogApi/src/test/java/org/fatoumi/exercice/controller/CommentControllerIntegrationTest.java
@@ -1,13 +1,23 @@
 package org.fatoumi.exercice.controller;
 
+import org.fatoumi.exercice.service.DefaultCommentService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(CommentController.class)
 public class CommentControllerIntegrationTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private DefaultCommentService commentService;
 
     @Test
     public void test() {
